acf669182d38e60e250edec0aa54d7190cace96b
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-02-10 20:17:15 +0530

    impl of `RTA` interface and derive macro w/ example

--- rta/src/lib.rs
+++ rta/src/lib.rs
@@ -1,1 +1,29 @@
+#![allow(unused)]
 
+pub use rta_derive::RTA;
+
+pub unsafe trait RTA: Clone + Sized {
+    const HASH: u64;
+    const SIZE: usize;
+}
+
+pub struct Rta<T: RTA> {
+    tp: T,
+}
+
+impl<T> Rta<T>
+where
+    T: RTA,
+{
+    pub fn new(tp: &T) -> Self {
+        Self { tp: tp.clone() }
+    }
+
+    pub fn size(&self) -> usize {
+        T::SIZE
+    }
+
+    pub fn hash(&self) -> u64 {
+        T::HASH
+    }
+}
