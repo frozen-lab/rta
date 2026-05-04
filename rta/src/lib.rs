698b4ca7eb6f3354a5f1db0b976e80e4c13af84a
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-05-04 14:00:57 +0530

    (rta) added module docs

--- rta/src/lib.rs
+++ rta/src/lib.rs
@@ -1,3 +1,70 @@
+//! Ṛta (ऋत) is a minimal metadata store for durable system state
+//!
+//! ## Requirements of `T`
+//!
+//! T must satisfy following layout and safety constraints:
+//!
+//! - Implements `RTA`
+//! - Uses `#[repr(C)]`
+//! - 8 bytes alignment
+//! - Does not implements `Drop`
+//! - Size must be >0 and multiple of 8
+//! - Implements `Default + Clone + Send + Sync + 'static`
+//!
+//! ## Limitations of `T`
+//!
+//! - No non-deterministic feilds
+//! - No interior pointers or self-references
+//! - Changes made in struct layout break compatibility
+//!
+//! ## Example
+//!
+//! ```rs
+//! use rta::{Rta, RTA};
+//!
+//! #[repr(C)]
+//! #[derive(Default, Clone, Copy, RTA)]
+//! struct TestType {
+//!     a: u64,
+//!     b: u64,
+//! }
+//!
+//! const MOD_ID: u8 = 0;
+//! let rta = Rta::<TestType, MOD_ID>::new().unwrap();
+//!
+//! rta.write(|t| {
+//!   t.a = 0x20;
+//!   t.b = 0x40;
+//! }).unwrap();
+//!
+//! let val = rta.read().unwrap();
+//! assert_eq!(val.a, 0x30);
+//! assert_eq!(val.b, 0x40);
+//! ```
+//!
+//! ## Write Operations
+//!
+//! The [`Rta::write`] call is _fire-and-forget_. It mutates in-mem state and triggers durable flush via a background
+//! thread, providing close to none IO overhead for the caller.
+//!
+//! Multiple write call, when made rightly one-after-another, may coalesce into fewer disk writes.
+//!
+//! ## Read Operations
+//!
+//! The [`Rta::read`] returns the latest in-mem state. By default the read is optimistic and does not guarantee durability
+//! at read time.
+//!
+//! ## Concurrency Model
+//!
+//! | Operation        | Parallelism | Blocks Reads | Blocks Writes |
+//! |:-----------------|-------------|--------------|---------------|
+//! | **Read**         | Yes         | No           | No            |
+//! | **Write**        | Limited     | No           | Sometimes     |
+//!
+//! ## Etymology
+//!
+//! Ṛta (ऋत) is a *Vedic* concept of cosmic order, truth, and invariance, which inspires the design of `Rta`.
+
 use frozen_core::{
     crc32::Crc32C,
     error::{ErrCode, FrozenErr},
@@ -133,6 +200,9 @@ pub struct Rta<T: RTA + Send + Sync + Clone, const MOD_ID: u8> {
     core: sync::Arc<Core<T, MOD_ID>>,
     handle: Option<thread::JoinHandle<()>>,
 }
+
+unsafe impl<T, const MOD_ID: u8> Send for Rta<T, MOD_ID> where T: RTA + Default + Send + Sync + Clone + 'static {}
+unsafe impl<T, const MOD_ID: u8> Sync for Rta<T, MOD_ID> where T: RTA + Default + Send + Sync + Clone + 'static {}
 
 impl<T, const MOD_ID: u8> Rta<T, MOD_ID>
 where
